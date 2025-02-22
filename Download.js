c9d34f6e3a471a05808927f202225a19c94ade3e
Hampter <Bradbeeschwarz@gmail.com>
2025-02-22 09:26:24 -0800

    Added that file i was talking about.

---
+++ Download.js
@@ -0,0 +1,4 @@
+// Function to Download Repo ZIP
+        function downloadRepo() {
+            window.location.href = "https://github.com/Warp-Core-PW/Impact/archive/refs/heads/main.zip";
+        }
